29a3cc159c7ac1017c25bb27c8ea40b199c99069
Mauricio G <mau.gar.gonz@icloud.com>
2019-05-27 17:50:34 -0500

    one

---
+++ template_files/2807287.js
@@ -0,0 +1,11 @@
+/* d65ff03fe7fc6fd62c2b3c6c235016bca2024854 / 2019-05-18 10:29:24 +0000 */
+
+window._dcfg = {"account_id":104748,"account_uid":2807287,"success":true,"env":"production","url":"www.simplii.mx","domain":"simplii.mx","enable_third_party_cookies":false,"eu_consent_check_timezone":false,"pond_id":"drip-production","campaigns":[],"forms":[],"goals":[{"id":9752,"public_id":332470216,"url":"/registro/bienvenido*"}],"url_triggers":[],"product_triggers":[{"trigger_type":"viewed_product"}],"lead_settings":{"product_benefits_path":null,"pricing_path":null,"careers_path":null},"enable_session_tracking":false};
+
+(function() {
+  var dc = document.createElement('script');
+  dc.type = 'text/javascript'; dc.async = true;
+  dc.src = 'https://d14jnfavjicsbe.cloudfront.net/client.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(dc, s);
+})();
